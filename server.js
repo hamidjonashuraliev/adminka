4d0194042134a3993a3f9902b9e8421a3191fec3
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2025-01-19 00:25:21 +0900

    feat: dotenv & project configuration

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString =
-    "mongodb+srv://abuzayd:HH9yw0pe6L7NRlTI@cluster0.stxh8.mongodb.net/johnstore?retryWrites=true&w=majority&appName=Cluster0";
+const connectionString = process.env.MONGO_URL;
+
 mongodb.connect(
     connectionString,
     { useNewUrlParser: true, useUnifiedTopology: true },
@@ -13,9 +16,10 @@ mongodb.connect(
             console.log("Mongo DB connection succeed ");
             // console.log(client);
             module.exports = client;
+
             const app = require("./app");
             const server = http.createServer(app);
-            let PORT = 7010;
+            let PORT = process.env.PORT || 3001;
             server.listen(PORT, function () {
                 console.log(
                     `The server is running successfully on port:${PORT}, http://localhost:${PORT}`
