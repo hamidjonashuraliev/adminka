b046354301a8abee72c3b84549e9faf676624f66
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2025-01-18 22:29:59 +0900

    Loyihani Express qismini shakllantirib olish

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+
+
+//Mongo DB chaqirish
+const db = require("./server").db();
+const mongodb = require("mongodb");
+
+
+// 1: KIrish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: section  code
+
+// 3: views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+
+module.exports = app;
+
---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+    "mongodb+srv://abuzayd:HH9yw0pe6L7NRlTI@cluster0.stxh8.mongodb.net/johnstore?retryWrites=true&w=majority&appName=Cluster0";
+mongodb.connect(
+    connectionString,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+        if (err) console.log("ERROR on connection MongoDB");
+        else {
+            console.log("Mongo DB connection succeed ");
+            // console.log(client);
+            module.exports = client;
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 7010;
+            server.listen(PORT, function () {
+                console.log(
+                    `The server is running successfully on port:${PORT}, http://localhost:${PORT}`
+                );
+            });
+        }
+    }
+);
